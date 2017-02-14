eb8d8a6c230121ce03f1c3e50e0c749049d645d6
Calvin Vette <calvin@dragontale.com>
2017-02-14 17:16:02 -0500

    Lab 6 Part 2b

--- app/auth/RegisterController.js
+++ app/auth/RegisterController.js
@@ -4,6 +4,41 @@
   console.log("RegisterController assigned");
   function RegisterController($scope) {
     console.log("RegisterController Loaded");
+    $scope.userName = "";
+    $scope.password = "";
+    $scope.confirmPassword = "";
+    $scope.rememberMe = false;
+    $scope.email = "";
+    $scope.firstName = "";
+    $scope.lastName = false;
+    $scope.companyName = "";
+    $scope.address = "";
+    $scope.city = "";
+    $scope.state = "";
+    $scope.postalCode = "";
+    $scope.country = "";
 
+    $scope.successMessage = "";
+    $scope.errorMessage = "";
+
+    $scope.register = function() {
+      console.log("Registering user: " + $scope.userName);
+      if ($scope.password == $scope.confirmPassword) {
+        $scope.errorMessage = "Passwords do not match!";
+        $scope.successMessage = "";
+        console.log("password and confirm password do not match!");
+        return false;
+      }
+      var validUserNamePassword = ($scope.password == "superSecret");
+      console.log("Valid: " + validUserNamePassword);
+      if (validUserNamePassword) {
+        $scope.successMessage = "Successfully Registered";
+        $scope.errorMessage = "";
+      } else {
+        $scope.errorMessage = "Invalid userName/password!";
+        $scope.successMessage = "";
+      }
+      return validUserNamePassword;
+    }
   }
 })();
