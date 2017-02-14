0608f61d31ec132c319d996395d253112d15270a
Calvin Vette <calvin@dragontale.com>
2017-02-14 17:02:02 -0500

    Lab 6 Part 2

--- app/auth/LoginController.js
+++ app/auth/LoginController.js
@@ -6,14 +6,20 @@
     console.log("LoginController Loaded");
     $scope.userName = "";
     $scope.password = "";
+    $scope.rememberMe = false;
     $scope.successMessage = "";
+    $scope.errorMessage = "";
 
     $scope.login = function() {
       console.log("Logging in user: " + $scope.userName);
       var validUserNamePassword = ($scope.password == "superSecret");
       console.log("Valid: " + validUserNamePassword);
-      if (valid) {
-        $scope.successMessage = "Successfully Logged In"; 
+      if (validUserNamePassword) {
+        $scope.successMessage = "Successfully Logged In";
+        $scope.errorMessage = "";
+      } else {
+        $scope.errorMessage = "Invalid userName/password!";
+        $scope.successMessage = "";
       }
       return validUserNamePassword;
     }
