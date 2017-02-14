ff8a2f0f533f371bb773ce43b51e90561abd696f
Calvin Vette <calvin@dragontale.com>
2017-02-14 12:05:24 -0500

    Lab 4 converted controllers to IIFEs

--- app/main/aboutUsController.js
+++ app/main/aboutUsController.js
@@ -1,13 +1,16 @@
-var mod=angular.module("mainModule");
-mod.controller("aboutUsController", function($scope) {
-  console.log("In the About Us Controller");
-  $scope.company = {
-    name : "Weasley's Wizarding Wheezes",
-    streetAddress : "10 Spring Gardens",
-    city : "London, SW1A 2BN"
+(function() {
+  angular.module("mainModule").controller("aboutUsController", aboutUsController);
+  aboutUsController.$inject = ['$scope'];
+  function aboutUsController($scope) {
+    console.log("In the About Us Controller");
+    $scope.company = {
+      name : "Weasley's Wizarding Wheezes",
+      streetAddress : "10 Spring Gardens",
+      city : "London, SW1A 2BN"
+    };
+    $scope.today = new Date();
+    $scope.clickMe = function() {
+      console.log("I was clicked at " + new Date());
+    };
   };
-  $scope.today = new Date();
-  $scope.clickMe = function() {
-    console.log("I was clicked at " + new Date());
-  };
-});
+})();
