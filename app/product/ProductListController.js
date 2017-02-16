c5c75a0a15e47d5496f2e0dfba68884b14a2f3b2
Calvin Vette <calvin@dragontale.com>
2017-02-16 14:20:06 -0500

    Lab 10 Partial

--- app/product/ProductListController.js
+++ app/product/ProductListController.js
@@ -4,7 +4,15 @@
   function ProductListController($scope, $http, CategoryService, ProductService) {
     console.log("In ProductListController");
     $scope.category = null;
-    $scope.products = ProductService.getProducts();
+    $scope.products = [];
+    ProductService.getProducts().then(function(response) {
+      console.log("Got products: ");
+      // console.log(response.data);
+      $scope.products = response.data;
+    });
+    $scope.$watch("products", function() {
+      console.log("Products updated...");
+    })
 
     $scope.categories = CategoryService.getAllCategories();
     $scope.$watch("category", function() {
